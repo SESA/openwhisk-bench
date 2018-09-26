35e1a9ee0e59593f5c83bd3d0b53ba83ae263847
devoshm <devoshm@bu.edu>
2018-09-26 16:57:37 -0500

    Calculating Execution Rate

--- constants.go
+++ constants.go
@@ -1,17 +1,18 @@
 package main
 
 const (
-	BATCH        = "Batch"
-	USER_ID      = "UserID"
-	USER_AUTH    = "UserAuth"
-	FUNCTION_ID  = "FunctionID"
-	PARAMETER    = "Parameter"
-	SEQ          = "Seq"
-	CMD_RESULT   = "ActivationId, WaitTime, InitTime, RunTime"
-	ELAPSED_TIME = "ElapsedTime"
-	SUBMITTED_AT = "StartTime"
-	ENDED_AT     = "EndTime"
-	EXEC_RATE    = "ExecRate"
+	BATCH                    = "Batch"
+	USER_ID                  = "UserID"
+	USER_AUTH                = "UserAuth"
+	FUNCTION_ID              = "FunctionID"
+	PARAMETER                = "Parameter"
+	SEQ                      = "Seq"
+	CMD_RESULT               = "ActivationId, WaitTime, InitTime, RunTime"
+	ELAPSED_TIME             = "ElapsedTime"
+	ELAPSED_TIME_SINCE_START = "ElapsedTimeSinceStart"
+	SUBMITTED_AT             = "StartTime"
+	ENDED_AT                 = "EndTime"
+	EXEC_RATE                = "ExecRate"
 
 	OPEN_WHISK_CONCURRENCY_FACTOR = 64
 )
