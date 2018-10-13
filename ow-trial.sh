3a310f56e58e042b3f838e45f4bff68a5b7dd991
Jim Cadden <jmcadden@bu.edu>
2018-10-12 22:49:50 -0400

    Clean up output of trial runs, add timing

--- ow-trial.sh
+++ ow-trial.sh
@@ -1,47 +1,62 @@
 #!/bin/bash
 ######################################################################
-#  Openwhisk Trail Runner
+#  Openwhisk Benchmark Trail Runner
 #
 ######################################################################
 
 export OWBENCH=${WSK_BENCH:=$PWD/openwhisk-bench}
 export OWDEPLOY=${OW_DEPLOY:=$PWD/../ow-deploy.sh}
-export OWWRAP=${OW_WRAP:="go run *.go -q"}
+export OWWRAP=${OW_WRAP:="go run *.go -q "}
 
 export TRIALDATADIR=${TRIAL_DATA_DIR:=$PWD/data}
 
-export CMDPREFIX=${CMD_PREFIX:=echo "CMDR: "}
-export CMDPOSTFIX=${CMD_POSTFIX:=}
+export CMDPREFIX=${CMD_PREFIX:=""}
+export CMDPOSTFIX=${CMD_POSTFIX:="&> /dev/null"}
 
 if [[ ! -z $DEBUG ]]; then set -x; fi
 
-echo "> Begin OpenWhisk Benchmark Trial"
-
+echo "Begin OpenWhisk Benchmark Trial"
 
 function CMDR
 {
-  $CMDPREFIX $@ $CMDPOSTFIX
+  local CMD="$CMDPREFIX ${@} $CMDPOSTFIX"
+  /bin/bash -c "$CMD"
 }
 
 function SingleTrial
 {
+  local START_TIME=$SECONDS
   local TPATH=$1
   FILES=$(/bin/bash -c "ls $TPATH | grep csv")
   
-  echo "$FILES"
+  echo "Batch files: $FILES"
 
   TrialInit
 
-  CMDR $OWDEPLOY Boot
-  
-  # IF argument is a directory
-  for i in $FILES; 
-  do 
-    DoRun $TPATH$i; 
-    CMDR $OWDEPLOY Reboot
-  done
+  echo "$SECONDS: Booting OpenWhisk..."
+  CMDR $OWDEPLOY Boot 
 
-  CMDR $OWDEPLOY Shutdown
+  LAST_FILE=""
+  for f in $FILES
+  do
+  	if [ ! -z $LAST_FILE ]
+  	then
+  	        echo "$SECONDS: Processing batch file $LAST_FILE"
+   		DoRun $TPATH$LAST_FILE; 
+   		echo "$SECONDS: Rebooting OpenWhisk..."
+   		CMDR $OWDEPLOY Reboot 
+  	fi
+  	LAST_FILE=$f
+  done
+  if [ ! -z $LAST_FILE ]
+  then
+        echo "$SECONDS: Process final batch file $LAST_FILE"
+   	DoRun $TPATH$LAST_FILE; 
+  fi
+  echo "$SECONDS: Shutting down OpenWhisk..."
+  CMDR $OWDEPLOY Shutdown 
+  local ELAPSED_TIME=$(($SECONDS - $START_TIME))
+  echo "$SECONDS: Finished Trial in $ELAPSED_TIME seconds"
 }
 
 function TrialInit
@@ -55,27 +70,18 @@ function TrialInit
 
 function DoRun 
 {
-
   local NAME=$(/bin/bash -c "echo $1 | /usr/bin/cut -d '/' -f 2")
-  local FN=$TRIALID
   if [[ -n $SEUSS ]]; then
     FN="$TRIALPATH/seuss_$NAME"
   else 
     FN="$TRIALPATH/linux_$NAME"
   fi
-  
   CMDR touch $FN
+  echo -e "\t $FN"
+  local START_TIME=$SECONDS
   CMDR $OWWRAP --create --writeToFile --fileName $FN execFile $1
-  
-  #if successful
-  if [[ -n $SEUSS ]]; then
-    echo "> Finished SEUSS run: $NAME"
-  else
-    echo "> Finished LINUX run: $NAME"
-  fi
-  
-
-  
+  local ELAPSED_TIME=$(($SECONDS - $START_TIME))
+  echo -e "\tRun finished in $ELAPSED_TIME seconds"
 }
 
 #######################################################################
